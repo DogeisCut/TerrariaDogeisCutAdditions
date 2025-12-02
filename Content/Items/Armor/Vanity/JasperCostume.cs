3a72a7bbc52d5966ceae4a6aea4cde659aafe883
DogeisCut <23178844+DogeisCut@users.noreply.github.com>
2025-12-01 23:09:57 -0700

    Add Jasper Costume wings and tail vanity items

--- Content/Items/Armor/Vanity/JasperCostume.cs
+++ Content/Items/Armor/Vanity/JasperCostume.cs
@@ -1,6 +1,8 @@
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using System.Linq;
+using Terraria.DataStructures;
 
 namespace DogeisCutAdditions.Content.Items.Armor.Vanity
 {
@@ -28,6 +30,13 @@ namespace DogeisCutAdditions.Content.Items.Armor.Vanity
 			ArmorIDs.Body.Sets.HidesArms[Item.bodySlot] = true;
 		}
 
+		public override void Load() {
+			if (Main.netMode == NetmodeID.Server)
+                return;
+			
+			EquipLoader.AddEquipTexture(Mod, $"{Texture}_Tail", EquipType.Waist, this);
+        }
+
 		public override void SetDefaults() {
 			Item.width = 18;
 			Item.height = 18;
@@ -49,5 +58,33 @@ namespace DogeisCutAdditions.Content.Items.Armor.Vanity
 			Item.rare = ItemRarityID.Pink;
             Item.vanity = true;
         }
+	}
+
+	[AutoloadEquip(EquipType.Wings)]
+	public class JasperCostumeWings : ModItem
+	{
+		public override void SetStaticDefaults() {
+			// These wings use the same values as the solar wings
+			// Fly time: 180 ticks = 3 seconds
+			// Fly speed: 9
+			// Acceleration multiplier: 2.5
+			ArmorIDs.Wing.Sets.Stats[Item.wingSlot] = new WingStats(25);
+		}
+
+		public override void SetDefaults() {
+			Item.width = 22;
+			Item.height = 20;
+			Item.rare = ItemRarityID.Pink;
+			Item.accessory = true;
+		}
+
+		// public override void VerticalWingSpeeds(Player player, ref float ascentWhenFalling, ref float ascentWhenRising,
+		// 	ref float maxCanAscendMultiplier, ref float maxAscentMultiplier, ref float constantAscend) {
+		// 	ascentWhenFalling = 0.85f; // Falling glide speed
+		// 	ascentWhenRising = 0.15f; // Rising speed
+		// 	maxCanAscendMultiplier = 1f;
+		// 	maxAscentMultiplier = 3f;
+		// 	constantAscend = 0.135f;
+		// }
 	}
 }
